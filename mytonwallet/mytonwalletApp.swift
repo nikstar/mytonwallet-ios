4c18b784a017c8c0f8205fa7f0a8ca57e67cde7a
Nikita Starshinov <nstarshinov@yandex.ru>
2024-03-14 21:53:23 +0300

    Initial Commit

---
+++ mytonwallet/mytonwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  mytonwalletApp.swift
+//  mytonwallet
+//
+//  Created by nikstar on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct mytonwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
